be7aa1ab6f3d7fd0eb0a2c82fb7d172673495c8b
Manda Putra <mandaputra8@gmail.com>
2024-03-30 16:11:01 +0700

    feat: gameboard style

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { randomString } from './utils.ts'
 import './style.css'
+import './reset.css'
 
 declare global {
   interface Window {
@@ -7,6 +8,7 @@ declare global {
   }
 }
 
+type Writeable<T> = { -readonly [P in keyof T]: T[P] };
 interface GameState {
   GameStatus: 'RUNNING' | 'WIN' | 'LOSE' | 'NOT_STARTED'
   is_first_time: boolean;
@@ -28,11 +30,11 @@ const COLORS = [
 ]
 
 class Game {
-  board: Element;
+  board: Writeable<HTMLElement>;
   state: GameState;
 
   constructor() {
-    this.board = $('#board')!
+    this.board = $('#board')! as HTMLElement
     this.state = {
       GameStatus: 'NOT_STARTED',
       boxs: [],
@@ -42,12 +44,24 @@ class Game {
 
   initialize(boardSize: number) {
     for (let i = 0; i < boardSize * boardSize; i++) {
-      this.board.innerHTML += `<div id="${randomString(10)}"></div>`
+      this.board.innerHTML += `<span id="box" data-value="${randomString(7)}"></span>`
     }
+    this.applyGridStyle(boardSize)
+    this.colorPair(boardSize)
   }
 
   colorPair(boardSize: number) {
 
+  }
+
+  applyGridStyle(boardSize: number) {
+    this.board.style.height = '400px'
+    this.board.style.border = '1px solid black'
+    this.board.style.display = 'grid'
+    this.board.style['grid-template-rows'] = `repeat(${boardSize}, 1fr)`
+    this.board.style['grid-template-columns'] = `repeat(${boardSize}, 1fr)`
+    this.board.style['grid-column-gap'] = '0px'
+    this.board.style['grid-row-gap'] = '0px'
   }
 }
 
